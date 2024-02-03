15b86918b3be716cf406bdafefe4afec74bcb708
maximilianvz <max.p.vanzyl@gmail.com>
2024-02-02 19:43:48 -0500

    Create Sphinx documentation site
    *Overwrote Michelle's previous commit, "Initialize Sphinx-Doc (#50)"
    
    *Created GitHub Actions workflow to update website with each push to master
    
    *Deleted setup.py in favour of pyproject.toml
    
    *Added pre-commit functionality
    
    *Added editorconfig file setting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,24 +4,23 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import sphinx_rtd_theme
-
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
 
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
 
 # -- Project information -----------------------------------------------------
 
-project = 'AtomDB'
-copyright = '2022, QC-Devs Team'
-author = 'QC-Devs Team'
+project = "AtomDB"
+copyright = "2024, QC-Devs"
+author = "QC-Devs"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,19 +29,51 @@ author = 'QC-Devs Team'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.mathjax',
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "nbsphinx",
+    "nbsphinx_link",
+    "IPython.sphinxext.ipython_console_highlighting"
 ]
 
+# List of arguments to be passed to the kernel that executes the notebooks:
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'svg', 'pdf'}",
+    "--InlineBackend.rc=figure.dpi=200",
+]
+# nbsphinx_input_prompt = 'In [%s]:'
+# nbsphinx_output_prompt = 'Out[%s]:'
+# explicitly dis-/enabling notebook execution
+nbsphinx_execute = "never"
+
+# Don't add .txt suffix to source files:
+html_sourcelink_suffix = ""
+
+# This is processed by Jinja2 and inserted before each notebook
+
+# nbsphinx_prolog = r"""
+# nbsphinx_epilog = r"""
+# {% set docname = env.docname.split("/")[-1] %}
+# .. raw:: html
+# .. role:: raw-html(raw)
+#  :format: html
+# .. nbinfo::
+#  The corresponding file can be obtained from:
+#  - Jupyter Notebook: :download:`{{docname+".ipynb"}}`
+#  - Interactive Jupyter Notebook: :raw-html:`<a href="https://mybinder.org/v2/gh/theochem/procrustes/master?filepath=doc%2Fnotebooks%2F/{{ docname }}.ipynb"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>`    """
+
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -50,18 +81,43 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+html_style = "css/override.css"
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {"collapse_navigation": False}
 
 
-mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js'
+# -- Configuration for autodoc extensions ---------------------------------
 
-
-mathjax_config = {
-    'extensions': ['tex2jax.js'],
-    'jax': ['input/TeX', 'output/HTML-CSS'],
+autodoc_default_options = {
+    "undoc-members": True,
+    "show-inheritance": True,
+    "members": None,
+    "inherited-members": True,
+    "ignore-module-all": True,
 }
+
+
+add_module_names = False
+
+
+def autodoc_skip_member(app, what, name, obj, skip, options):
+    """Decide which parts to skip when building the API doc."""
+    if name == "__init__":
+        return False
+    if name.startswith("test_"):
+        return False
+    return skip
+
+
+def setup(app):
+    """Set up sphinx."""
+    app.connect("autodoc-skip-member", autodoc_skip_member)
