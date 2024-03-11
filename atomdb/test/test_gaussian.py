c7ae306f997ff0fde490ef95765f229e01b8d973
Marco Martínez González <mmg870630@gmail.com>
2024-03-10 23:29:45 -0400

    Add test for second derivative of rho function
    Added a test for the function that computes the second derivative of rho
    with R. The test works for spherical systems.

--- atomdb/test/test_gaussian.py
+++ atomdb/test/test_gaussian.py
@@ -74,6 +74,52 @@ def test_eval_radial_d_density(atom):
     assert np.allclose(rho_grad, rho_grad_cart, rtol=1e-6)
 
 
+
+@pytest.mark.dev
+@pytest.mark.parametrize(
+    "atom", ["atom_018_Ar_N18_M1_uhf_def2svpd_g09.fchk", "atom_001_H_N02_M1_uhf_def2svpd_g09.fchk"]
+)
+def test_eval_radial_dd_density(atom):
+    # conditional import of dev modules needed for this test
+    try:
+        from gbasis.wrappers import from_iodata
+        from gbasis.evals.density import evaluate_density_gradient, evaluate_density_hessian
+        from gbasis.evals.density import evaluate_density_laplacian
+
+        from grid import UniformInteger, LinearInfiniteRTransform, AtomGrid
+        from iodata import load_one
+        from atomdb.datasets.gaussian import eval_radial_d_density, eval_radial_dd_density
+
+        assert True
+    except ImportError:
+        pytest.skip("Gbasis or IOData modules not available, skipping test")
+
+    # create atomic grid from 0 to 20 bohr
+    oned = UniformInteger(npoints=100)
+    rgrid = LinearInfiniteRTransform(1e-4, 20).transform_1d_grid(oned)
+    atgrid = AtomGrid(rgrid, degrees=[10])
+
+    # load the fchk file
+    mol_data = load_one(TEST_DATAPATH + "gaussian/" + atom)
+    ao_basis = from_iodata(mol_data)
+
+    # one electron RDM from fchk file
+    rdm = mol_data.one_rdms["scf"]
+
+    # evaluate fisrt and second derivatives of rho vs r on the grid
+    radial_d_rho = eval_radial_d_density(rdm, ao_basis, atgrid.points)
+    radial_dd_rho = eval_radial_dd_density(rdm, ao_basis, atgrid.points)
+
+    # evaluate gradient, hessian and laplacian of the electron density on the grid
+    rho_lapl = evaluate_density_laplacian(rdm, ao_basis, atgrid.points)
+
+    # compute laplacian from first and second radial derivatives (for spheric atoms) and compare
+    rho_lapl_rec = radial_dd_rho + 2 * radial_d_rho / np.linalg.norm(atgrid.points, axis=1)
+    assert np.allclose(rho_lapl, rho_lapl_rec, rtol=1e-6)
+
+
+
+
 def test_compiled_gaussian_hf_data():
     ### Use Be atomic data as a test case
     sp = load("Be", 0, 1, dataset="gaussian", datapath=TEST_DATAPATH)
